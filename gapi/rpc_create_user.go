a48846324b9bc75d82d1749a329ba0cbe508d25e
Dmitriy Arbitman <d.arbitman@gmail.com>
2023-06-13 16:15:22 -0700

    Adding tasks to redis to create a new user. Will add sending an email to verify soon.

--- gapi/rpc_create_user.go
+++ gapi/rpc_create_user.go
@@ -2,11 +2,14 @@ package gapi
 
 import (
 	"context"
+	"time"
 
 	db "github.com/darbitman/simplebank/db/sqlc"
 	"github.com/darbitman/simplebank/pb"
 	"github.com/darbitman/simplebank/util"
 	"github.com/darbitman/simplebank/val"
+	"github.com/darbitman/simplebank/worker"
+	"github.com/hibiken/asynq"
 	"github.com/lib/pq"
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
@@ -40,6 +43,20 @@ func (server *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest)
 			}
 		}
 		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err)
+	}
+
+	// TODO use db transaction
+	taskPayload := &worker.PayloadSendVerifyEmail{
+		Username: user.Username,
+	}
+	opts := []asynq.Option{
+		asynq.MaxRetry(10),
+		asynq.ProcessIn(10 * time.Second),
+		asynq.Queue(worker.QueueCritical),
+	}
+	err = server.taskDistributor.DistributeTaskSendVerifyEmail(ctx, taskPayload, opts...)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to distribute task to send verify email: %s", err)
 	}
 
 	rsp := &pb.CreateUserResponse{
