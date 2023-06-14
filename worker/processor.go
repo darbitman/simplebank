39e4bb4cd858958734a4566b32d7c78463e7c425
Dmitriy Arbitman <d.arbitman@gmail.com>
2023-06-14 16:07:34 -0700

    Added custom logger to asynq to conform to the format of the simple-bank service logs.

--- worker/processor.go
+++ worker/processor.go
@@ -5,6 +5,7 @@ import (
 
 	db "github.com/darbitman/simplebank/db/sqlc"
 	"github.com/hibiken/asynq"
+	"github.com/rs/zerolog/log"
 )
 
 const (
@@ -22,13 +23,23 @@ type RedisTaskProcessor struct {
 	store  db.Store
 }
 
-func NewRedisTasProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskProcessor {
-	server := asynq.NewServer(redisOpt, asynq.Config{
-		Queues: map[string]int{
-			QueueCritical: 10,
-			QueueDefault:  5,
-		},
-	})
+func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.Store) TaskProcessor {
+	server := asynq.NewServer(
+		redisOpt,
+		asynq.Config{
+			Queues: map[string]int{
+				QueueCritical: 10,
+				QueueDefault:  5,
+			},
+			ErrorHandler: asynq.ErrorHandlerFunc(func(ctx context.Context, task *asynq.Task, err error) {
+				log.Error().
+					Err(err).
+					Str("type", task.Type()).
+					Bytes("payload", task.Payload()).
+					Msg("process task failed")
+			}),
+			Logger: NewLogger(),
+		})
 
 	return &RedisTaskProcessor{
 		server: server,
