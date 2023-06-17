131edc412be573c4fc16c8ce52a080382c5b9068
Dmitriy Arbitman <d.arbitman@gmail.com>
2023-06-17 12:10:52 -0700

    Add unit test to check UpdateUser grpc api.

--- gapi/main_test.go
+++ gapi/main_test.go
@@ -1,13 +1,17 @@
 package gapi
 
 import (
+	"context"
+	"fmt"
 	"testing"
 	"time"
 
 	db "github.com/darbitman/simplebank/db/sqlc"
+	"github.com/darbitman/simplebank/token"
 	"github.com/darbitman/simplebank/util"
 	"github.com/darbitman/simplebank/worker"
 	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/metadata"
 )
 
 func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDistributor) *Server {
@@ -15,8 +19,23 @@ func newTestServer(t *testing.T, store db.Store, taskDistributor worker.TaskDist
 		TokenSymmetricKey:   util.RandomString(32),
 		AccessTokenDuration: time.Minute,
 	}
+
 	server, err := NewServer(config, store, taskDistributor)
 	require.NoError(t, err)
 
 	return server
+}
+
+func newContextWithBearerToken(t *testing.T, tokenMaker token.Maker, username string, duration time.Duration) context.Context {
+	accessToken, payload, err := tokenMaker.CreateToken(username, duration)
+	require.NoError(t, err)
+	_ = payload
+
+	bearerToken := fmt.Sprintf("%s %s", authorizationBearer, accessToken)
+	md := metadata.MD{
+		authorizationHeader: []string{
+			bearerToken,
+		},
+	}
+	return metadata.NewIncomingContext(context.Background(), md)
 }
